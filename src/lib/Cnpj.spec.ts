4c24290e60fa863b8adf4c143843dfa1e8ccc784
Enricky Hipólito <enricky.hipolito@gmail.com>
2022-12-23 19:34:08 -0300

    create format and cleanUp methods

--- src/lib/Cnpj.spec.ts
+++ src/lib/Cnpj.spec.ts
@@ -20,4 +20,29 @@ describe("Cnpj", () => {
       expect(Cnpj.validateFormat("111.111.111-11")).toBe(false);
     });
   });
+
+  describe("cleanUp", () => {
+    it("should clean up cnpj", () => {
+      expect(Cnpj.cleanUp("27.103.239/0001-56")).toBe("27103239000156");
+      expect(Cnpj.cleanUp("649.98136054123")).toBe("64998136054123");
+      expect(Cnpj.cleanUp("65-280-961-0001-43")).toBe("65280961000143");
+      expect(Cnpj.cleanUp("289 asasa88a  43w2sassa7.56as002")).toBe("28988432756002");
+    });
+  });
+
+  describe("format", () => {
+    it("should format cnpj", () => {
+      expect(Cnpj.format("27303239456634")).toBe("27.303.239/4566-34");
+      expect(Cnpj.format("649.98136054354")).toBe("64.998.136/0543-54");
+      expect(Cnpj.format("65-280-961-0001-43")).toBe("65.280.961/0001-43");
+      expect(Cnpj.format("289 asasa88a   43w2sassa7.56as002")).toBe("28.988.432/7560-02");
+    });
+
+    it("should return null if is not possible to format", () => {
+      expect(Cnpj.format("965563860012")).toBeNull();
+      expect(Cnpj.format("aaaa")).toBeNull();
+      expect(Cnpj.format("23.547.298/6663-4")).toBeNull();
+      expect(Cnpj.format("aa.aaa.aaa/aaaa-aa")).toBeNull();
+    });
+  });
 });
--- src/lib/Cnpj.ts
+++ src/lib/Cnpj.ts
@@ -4,4 +4,14 @@ export default class Cnpj {
   public static validateFormat(cnpj: string): boolean {
     return Cnpj.regex.test(cnpj);
   }
+
+  public static cleanUp(cnpj: string): string {
+    return cnpj.replace(/\D+/g, ""); //remove tudo que não é digito
+  }
+
+  public static format(cnpj: string) {
+    const cleanCnpj = Cnpj.cleanUp(cnpj);
+    if (cleanCnpj.length < 14) return null;
+    return cleanCnpj.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})$/, "$1.$2.$3/$4-$5"); //$1, $2, ... -> referem-se a cada parenteses do regex
+  }
 }
